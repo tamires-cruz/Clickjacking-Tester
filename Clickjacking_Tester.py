e1b4f7a1b21f41bb3604b6739e163b543102b983
tamires-cruz <69864264+tamires-cruz@users.noreply.github.com>
2023-06-15 10:54:15 -0300

    Update Clickjacking_Tester.py
    just updating the POC generation, as it generated errors and did not create the file.

--- Clickjacking_Tester.py
+++ Clickjacking_Tester.py
@@ -1,7 +1,14 @@
 # Contributor(s): nigella (@nig)
 
+# Recomend to install the libs.
+# pip install urllib3
+# pip install sys
+# How to use: python3 Clickjacking_Tester.py listofsites.txt
+
+
 from urllib.request import urlopen
 from sys import argv, exit
+import os
 
 __author__ = 'D4Vinci'
 
@@ -9,14 +16,19 @@ def check(url):
     ''' check given URL is vulnerable or not '''
 
     try:
-        if "http" not in url: url = "http://" + url
+        if "http" not in url:
+            url = "http://" + url
 
         data = urlopen(url)
         headers = data.info()
 
-        if not "X-Frame-Options" in headers: return True
+        if not "X-Frame-Options" in headers:
+            return True
 
-    except: return False
+    except:
+        return False
+
+
 
 
 def create_poc(url):
@@ -32,16 +44,24 @@ def create_poc(url):
 </html>
     """.format(url)
 
-    with open(url + ".html", "w") as f:
+    file_name = os.path.basename(url) + ".html"  # Obtém apenas o nome do arquivo e adiciona a extensão ".html"
+    file_path = os.path.join(os.getcwd(), file_name)  # Cria o caminho absoluto para o arquivo
+
+    with open(file_path, "w") as f:
         f.write(code)
         f.close()
-
 
 def main():
     ''' Everything comes together '''
 
-    try: sites = open(argv[1], 'r').readlines()
-    except: print("[*] Usage: python(3) clickjacking_tester.py <file_name>"); exit(0)
+    try:
+        sites = open(argv[1], 'r').readlines()
+    except FileNotFoundError:
+        print("[*] Arquivo não encontrado. Certifique-se de fornecer um arquivo válido como argumento.")
+        exit(0)
+    except Exception as e:
+        print("[*] Ocorreu um erro ao ler o arquivo de sites:", str(e))
+        exit(0)
 
     for site in sites[0:]:
         print("\n[*] Checking " + site)
@@ -52,7 +72,10 @@ def main():
             create_poc(site.split('\n')[0])
             print(" [*] Created a poc and saved to <URL>.html")
 
-        elif not status: print(" [-] Website is not vulnerable!")
-        else: print('Every single thing is crashed, Python got mad, dude wtf you just did?')
+        elif not status:
+            print(" [-] Website is not vulnerable!")
+        else:
+            print('Every single thing is crashed, Python got mad, dude wtf you just did?')
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
